1dd6f459041f636f6c403f76f183cc24f20af830
jmgomez <info@jmgomez.me>
2022-09-21 19:14:12 +0100

    extracts the genBindings func so it takes a module name

--- src/nimforue/examples/examplescodegen.nim
+++ src/nimforue/examples/examplescodegen.nim
@@ -6,53 +6,63 @@ import ../macros/makestrproc
 
 import ../../codegen/codegentemplate
 
+
+proc genBindings(moduleName:string, moduleRules:seq[UEImportRule]) =
+  let config = getNimForUEConfig()
+  let reflectionDataPath = config.pluginDir / "src" / ".reflectiondata" #temporary
+  createDir(reflectionDataPath)
+  let bindingsDir = config.pluginDir / "src"/"nimforue"/"unreal"/"bindings"
+  createDir(bindingsDir)
+  
+  var module = tryGetPackageByName(moduleName)
+                      .flatmap((pkg:UPackagePtr) => pkg.toUEModule(moduleRules))
+                      .get()
+  let codegenPath = reflectionDataPath / moduleName.toLower() & ".nim"
+  let exportBindingsPath = bindingsDir / "exported" / moduleName.toLower() & ".nim"
+  let importBindingsPath = bindingsDir / moduleName.toLower() & ".nim"
+  UE_Log &"-= The codegen module path is {codegenPath} =-"
+
+  try:
+    let codegenTemplate = codegenNimTemplate % [$module, escape(exportBindingsPath), escape(importBindingsPath)]
+    #UE_Warn &"{codegenTemplate}"
+    writeFile(codegenPath, codegenTemplate)
+    let nueCmd = config.pluginDir/"nue.exe codegen --module:\"" & codegenPath & "\""
+    let result = execProcess(nueCmd, workingDir = config.pluginDir)
+    # removeFile(codegenPath)
+    UE_Log &"The result is {result} "
+    UE_Log &"-= Bindings for {moduleName} generated in {exportBindingsPath} =- "
+
+    doAssert(fileExists(exportBindingsPath))
+    doAssert(fileExists(importBindingsPath))
+  except:
+    let e : ref Exception = getCurrentException()
+
+    UE_Log &"Error: {e.msg}"
+    UE_Log &"Error: {e.getStackTrace()}"
+    UE_Log &"Failed to generate {codegenPath} nim binding"
+
+
 #This is just for testing/exploring, it wont be an actor
 uClass AActorCodegen of AActor:
   (BlueprintType)
   ufuncs(CallInEditor):
     proc generateUETypes() = 
       # let a = ETest.testB
-      let config = getNimForUEConfig()
-      let reflectionDataPath = config.pluginDir / "src" / ".reflectiondata" #temporary
-      createDir(reflectionDataPath)
-      let bindingsDir = config.pluginDir / "src"/"nimforue"/"unreal"/"bindings"
-      createDir(bindingsDir)
-      let moduleNames = @["NimForUEBindings", "Engine"]
-      # let moduleNames = @["NimForUEBindings"]
       let moduleRules = @[
-          makeImportedRuleType(uerCodeGenOnlyFields, @["AActor", "UReflectionHelpers"]), 
-          makeImportedRuleField(uerIgnore, @["PerInstanceSMCustomData", "PerInstanceSMData" ]) #Enum not working because of the TEnum constructor being redefined by nim and it was already defined in UE. The solution would be to just dont work with TEnumAsByte but with the Enum itself which is more convenient. 
+        makeImportedRuleType(uerCodeGenOnlyFields, @["AActor", "UReflectionHelpers"]), 
+        makeImportedRuleField(uerIgnore, @["PerInstanceSMCustomData", "PerInstanceSMData" ]) #Enum not working because of the TEnum constructor being redefined by nim and it was already defined in UE. The solution would be to just dont work with TEnumAsByte but with the Enum itself which is more convenient. 
 
-        ]
-      # let moduleNames = @["NimForUEBindings"]
+      ] 
+      let moduleNames = @["Engine"]
       for moduleName in moduleNames:
-        var module = tryGetPackageByName(moduleName)
+        var engineModule = tryGetPackageByName(moduleName)
                       .flatmap((pkg:UPackagePtr) => pkg.toUEModule(moduleRules))
                       .get()
+        
+        for moduleName in engineModule.dependencies:
+          genBindings(moduleName, moduleRules)
 
-        let codegenPath = reflectionDataPath / moduleName.toLower() & ".nim"
-        let exportBindingsPath = bindingsDir / "exported" / moduleName.toLower() & ".nim"
-        let importBindingsPath = bindingsDir / moduleName.toLower() & ".nim"
-        UE_Log &"-= The codegen module path is {codegenPath} =-"
-
-        try:
-          let codegenTemplate = codegenNimTemplate % [$module, escape(exportBindingsPath), escape(importBindingsPath)]
-          #UE_Warn &"{codegenTemplate}"
-          writeFile(codegenPath, codegenTemplate)
-          let nueCmd = config.pluginDir/"nue.exe codegen --module:\"" & codegenPath & "\""
-          let result = execProcess(nueCmd, workingDir = config.pluginDir)
-          # removeFile(codegenPath)
-          UE_Log &"The result is {result} "
-          UE_Log &"-= Bindings for {moduleName} generated in {exportBindingsPath} =- "
-
-          doAssert(fileExists(exportBindingsPath))
-          doAssert(fileExists(importBindingsPath))
-        except:
-          let e : ref Exception = getCurrentException()
-
-          UE_Log &"Error: {e.msg}"
-          UE_Log &"Error: {e.getStackTrace()}"
-          UE_Log &"Failed to generate {codegenPath} nim binding"
+        
 
 
     proc showEngineDependencies() = 
