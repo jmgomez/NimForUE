f535979aa035b30a32c44efe1e8e7da6e9da39e7
DevKevinYG <kevinyabargarces@outlook.com>
2023-07-02 16:30:37 -0500

    Added some bindings for TSets

--- src/nimforue/unreal/core/containers/set.nim
+++ src/nimforue/unreal/core/containers/set.nim
@@ -1,4 +1,12 @@
 include ../../definitions
+import array
 
 type TSet*[T] {.importcpp.} = object
-  
+
+func num*[T](setparam:TSet[T]): int32 {.importcpp: "#.Num()" .}
+proc remove*[T](setparam:TSet[T], value:T) {.importcpp: "#.Remove(#)".}
+proc add*[T](setparam:TSet[T], value:T) {.importcpp: "#.Add(#)".}
+proc findOrAdd*[T](setparam:TSet[T], value:T) : T {.importcpp: "#.FindOrAdd(#)".}
+func reserve*[T](setparam:TSet[T], value:int32) {.importcpp: "#.Reserve(#)".}
+
+proc toArray*[T](setparam:TSet[T]) : TArray[T] {.importcpp: "#.Array()" .}
