0b63b2c96dbf299b020a900a0cce6ac22bd3a8eb
jmgomez <info@jmgomez.me>
2023-07-18 17:05:24 +0100

    improves nimsuggest

--- src/nimforue/codegen/projectinstrospect.nim
+++ src/nimforue/codegen/projectinstrospect.nim
@@ -761,7 +761,7 @@ proc getAllModulesFrom(dir, entryPoint:string) : seq[NimModule] =
 
 #todo cache to a file
 when not defined(game) or defined(vmhost):
-  when not defined(nimcheck):
+  when not defined(nimcheck) and not defined(nimsuggest):
     const dir = PluginDir / "src" / "nimforue" / "unreal" 
     const entryPoint = dir / "prelude.nim"
     # const dir = PluginDir / "src" / "nimforue" / "unreal" / "engine" 
