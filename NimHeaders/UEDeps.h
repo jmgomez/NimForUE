134eca36e236a639fa2f685eec40b1ecbe3c771d
jmgomez <info@jmgomez.me>
2023-11-19 16:15:58 +0000

    Progress. Autopackage compiles but doesnt link yet

--- NimHeaders/UEDeps.h
+++ NimHeaders/UEDeps.h
@@ -92,9 +92,8 @@
   #include "AssetTypeActions_Base.h"
 
 #endif
-#include "NavigationSystem.h"
 #include "Components/Widget.h"
-#include "UMG/Public/Components/Viewport.h"
+#include "Components/Viewport.h"
 #include "Widgets/Docking/SDockTab.h"
 
 
@@ -105,6 +104,7 @@
 #include "AbilitySystemComponent.h"
 #include "ABilitySystemInterface.h"
 #include "Abilities/Tasks/AbilityTask.h"
+#include "NavigationSystem.h"
 
 #if  ENGINE_MINOR_VERSION >= 2   
 #include "Elements/PCGExecuteBlueprint.h"
