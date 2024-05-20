7def2a04199d8260256ff7a122fa72d6e6dc79a2
jmgomez <info@jmgomez.me>
2024-05-20 13:00:54 +0100

    fixes #37

--- src/nimforue/codegen/uemeta.nim
+++ src/nimforue/codegen/uemeta.nim
@@ -962,8 +962,8 @@ proc emitUClass*[T](ueType: UEType, package: UPackagePtr, fnTable: seq[FnEmitter
 
   discard newCls.getDefaultObject()#forces the creation of the cdo. the LC reinstancer needs it created before the object gets nulled out
     # broadcastAsset(newCls) Dont think this is needed since the notification will be done in the boundary of the plugin
-  if newCls.isChildOf[:UEngineSubsystem]():
-    UE_Warn &"Activating engine subsystem {newCls.getName()}"
+  if newCls.isChildOf[:UDynamicSubsystem]():
+    log &"Activating engine subsystem {newCls.getName()}"
     #initi the subystem. Doesnt seem that we need to deactivated it but there is an inverse.
     #Need to test if it's neccesary with non engine subsystems
     activateExternalSubsystem(newCls)
