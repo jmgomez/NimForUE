4f2cfad279ca2505d056e2ed43e99e2a3bbe9d0b
info@jmgomez.me <info@jmgomez.me>
2024-10-13 15:14:04 +0100

    Adds mvvm helpers

--- src/nimforue/game/extras/modelviewviewmodel.nim
+++ src/nimforue/game/extras/modelviewviewmodel.nim
@@ -22,3 +22,37 @@ proc broadcastFieldValueChanged*(vm: UMVVMViewModelBasePtr, fieldId: FFieldId): 
 proc broadcastFieldValueChanged*(vm: UMVVMViewModelBasePtr, name: FName): void = 
   let fieldId = getFieldId(vm, name)
   broadcastFieldValueChanged(vm, fieldId)
+
+#[
+These includes should be in nuegame.h in order to make the following work:
+#include "MVVMViewModelBase.h"
+#include "Types/MVVMViewModelContext.h"
+
+]#
+proc getViewModelCollectionWithContextFor(T: typedesc, worldContext: UObjectPtr): (UMVVMViewModelCollectionObjectPtr, FMVVMViewModelContext)   =
+  let vmSubsystem = tryGetSubsystem[UMVVMGameSubsystem](worldContext).get(nil)
+  if vmSubsystem.isNil:
+    # printString "ViewModel Subsystem not found"
+    return (nil, FMVVMViewModelContext())
+  let vmContext = FMVVMViewModelContext(
+    contextClass: makeTSubclassOf[UMVVMViewModelBase](T.staticClass),
+    contextName: T.staticClass.getFName()
+  )
+  let col = vmSubsystem.get.getViewModelCollection()
+  (col, vmContext)
+
+proc addViewModelToCollection*(T: typedesc, worldContext: UObjectPtr): ptr T  =
+  let (collection, vmContext) = getViewModelCollectionWithContextFor(T, worldContext)
+  if collection.isNil: return nil
+  let instance = newUObject[T]()
+  discard collection.addViewModelInstance(vmContext, instance)
+  instance
+
+proc getViewModelFromCollection*(T: typedesc, worldContext: UObjectPtr): ptr T  = 
+  let (collection, vmContext) = getViewModelCollectionWithContextFor(T, worldContext)
+  if collection.isNil: return nil
+  let instance = collection.findViewModelInstance(vmContext).ueCast(T)
+  if instance.isNil:
+    addViewModelToCollection(T, worldContext)
+  else:
+    instance
