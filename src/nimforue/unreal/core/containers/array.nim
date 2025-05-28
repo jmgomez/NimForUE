16ecc6cd52fb3ced9b0b600d5b47e8faec78437a
Don-Duong Quach <geekrelief@gmail.com>
2025-05-27 18:55:38 -0700

    Fixed bug in TArray push, added TArray reset() and empty()

--- src/nimforue/unreal/core/containers/array.nim
+++ src/nimforue/unreal/core/containers/array.nim
@@ -14,8 +14,10 @@ proc append*[T](a, b:TArray[T]) {.importcpp: "#.Append(#)".}
 func reserve*[T](arr:TArray[T], value:Natural) {.importcpp: "#.Reserve(#)".}
 func indexOf*[T](arr:TArray[T], value:T): int32 {.importcpp: "#.IndexOfByKey(#)"}
 func pop*[T](arr:TArray[T]): T {.importcpp: "#.Pop()"}
-func push*[T](arr:TArray[T], value: T): T {.importcpp: "#.Push()"}
+func push*[T](arr:TArray[T], value: T): T {.importcpp: "#.Push(@)"}
 func sort*[T](arr:TArray[T]) {.importcpp: "#.Sort()"}
+func reset*[T](arr:TArray[T]) {.importcpp: "#.Reset()"}
+func empty*[T](arr:TArray[T]) {.importcpp: "#.Empty()"}
 
 
 # proc `[]`*[T](arr:TArray[T], i: int): var T {. inline, noSideEffect.} = arr[i.int32]
